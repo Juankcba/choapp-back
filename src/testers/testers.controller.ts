5a98a62fa41f8fe078130b37cc7d7ffa6ae07c89
juankcba <juangblade@hotmail.com>
2026-04-16 10:41:00 -0300

    changes

--- src/testers/testers.controller.ts
+++ src/testers/testers.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { TestersService } from './testers.service';
+import { Public } from '../auth/decorators';
 
 @Controller('testers')
 export class TestersController {
     constructor(private readonly testersService: TestersService) { }
 
+    @Public()
     @Post('register')
     async register(
         @Body() body: {
